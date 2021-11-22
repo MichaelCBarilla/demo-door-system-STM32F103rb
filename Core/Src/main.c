6012a9ba6be615bc907038698f6a48627bff6b47
michaelcbarilla <michaelcbarilla@gmail.com>
2021-11-22 16:49:36 -0500

    refactor pin layout variables, create struct to hold pin number and letter, re arrange pins to make board more clean

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -30,19 +30,15 @@
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-#define DOOR_1_RED_LED	 GPIO_PIN_12
-#define DOOR_1_GREEN_LED GPIO_PIN_11
-#define DOOR_1_OUTSIDE_SWITCH    GPIO_PIN_7
-#define DOOR_2_GREEN_LED GPIO_PIN_10
-#define DOOR_2_RED_LED   GPIO_PIN_9
-#define DOOR_2_OUTSIDE_SWITCH    GPIO_PIN_8
-#define PUSH_TO_LOCK 		 GPIO_PIN_6
-#define EMERGENCY 		 GPIO_PIN_4
 
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+struct Pin_Layout {
+	 GPIO_TypeDef *GPIOx;
+   unsigned short int  GPIO_Pin;
+};
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -54,6 +50,22 @@
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
+
+/* Door 1 Pin Layouts. */
+const struct Pin_Layout DOOR_1_RED_LED = {GPIOC, GPIO_PIN_12};
+const struct Pin_Layout DOOR_1_GREEN_LED = {GPIOC, GPIO_PIN_11};
+const struct Pin_Layout DOOR_1_OUTSIDE_SWITCH = {GPIOC, GPIO_PIN_10};
+const struct Pin_Layout DOOR_1_INSIDE_SWITCH = {GPIOD, GPIO_PIN_2};
+
+/* Door 2 Pin Layouts. */
+const struct Pin_Layout DOOR_2_RED_LED = {GPIOA, GPIO_PIN_9};
+const struct Pin_Layout DOOR_2_GREEN_LED = {GPIOC, GPIO_PIN_0};
+const struct Pin_Layout DOOR_2_OUTSIDE_SWITCH = {GPIOC, GPIO_PIN_3};
+const struct Pin_Layout DOOR_2_INSIDE_SWITCH = {GPIOC, GPIO_PIN_1};
+
+/* Misc. Switches */
+const struct Pin_Layout PUSH_TO_LOCK = {GPIOA, GPIO_PIN_12};
+const struct Pin_Layout EMERGENCY = {GPIOC, GPIO_PIN_8};
 
 /* USER CODE END PV */
 
@@ -228,8 +240,10 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PA4 PA6 PA7 PA8 */
-  GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8;
+  /*Configure GPIO pins : PA1 PA4 PA6 PA7
+                           PA8 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_4|GPIO_PIN_6|GPIO_PIN_7
+                          |GPIO_PIN_8;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
@@ -243,6 +257,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
+  /*Configure GPIO pin : PB0 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
   /* EXTI interrupt init*/
   HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
@@ -252,7 +272,7 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 void is_pushed_to_lock_pressed(bool *door_1_unlocked_ptr, bool *door_2_unlocked_ptr, bool *waiting_on_ptl_release_ptr, bool *waiting_on_door_close_ptr)
 {
-	bool pressed_down = HAL_GPIO_ReadPin(GPIOA, PUSH_TO_LOCK) == 0;
+	bool pressed_down = HAL_GPIO_ReadPin(PUSH_TO_LOCK.GPIOx, PUSH_TO_LOCK.GPIO_Pin) == 0;
 
 	if (!pressed_down && *waiting_on_ptl_release_ptr && !*waiting_on_door_close_ptr) {
 		*door_1_unlocked_ptr = false;
@@ -265,7 +285,7 @@ void is_pushed_to_lock_pressed(bool *door_1_unlocked_ptr, bool *door_2_unlocked_
 
 void is_emergency_pressed(bool *door_1_unlocked_ptr, bool *door_2_unlocked_ptr, bool *waiting_on_emergency_release_ptr)
 {
-	bool pressed_down = HAL_GPIO_ReadPin(GPIOA, EMERGENCY) == 0;
+	bool pressed_down = HAL_GPIO_ReadPin(EMERGENCY.GPIOx, EMERGENCY.GPIO_Pin) == 0;
 
 	if (!pressed_down && *waiting_on_emergency_release_ptr) {
 		*door_1_unlocked_ptr = true;
@@ -278,10 +298,17 @@ void is_emergency_pressed(bool *door_1_unlocked_ptr, bool *door_2_unlocked_ptr, 
 
 void check_door_1_status(bool *door_1_unlocked_ptr, bool *door_2_unlocked_ptr, bool *waiting_on_door_close_ptr)
 {
+	bool opening_door_1_from_inside = HAL_GPIO_ReadPin(DOOR_1_INSIDE_SWITCH.GPIOx, DOOR_1_INSIDE_SWITCH.GPIO_Pin) == 0;
+
+	/* Check if opening door 1 from inside */
+	if (opening_door_1_from_inside) {
+
+	}
+
 	/* Check if door 1 is unlocked. */
 	if (*door_1_unlocked_ptr) {
 
-		bool door_1_opened = HAL_GPIO_ReadPin(GPIOA, DOOR_1_OUTSIDE_SWITCH) == 0;
+		bool door_1_opened = HAL_GPIO_ReadPin(DOOR_1_OUTSIDE_SWITCH.GPIOx, DOOR_1_OUTSIDE_SWITCH.GPIO_Pin) == 0;
 
 		/* Set boolean to tell push to lock to wait for door to close before locking */
 		*waiting_on_door_close_ptr = door_1_opened;
@@ -300,11 +327,11 @@ void check_door_1_status(bool *door_1_unlocked_ptr, bool *door_2_unlocked_ptr, b
 void update_door_1_leds(bool *door_1_unlocked_ptr)
 {
 	if (*door_1_unlocked_ptr) {
-		HAL_GPIO_WritePin(GPIOA, DOOR_1_RED_LED, 0);
-		HAL_GPIO_WritePin(GPIOA, DOOR_1_GREEN_LED, 1);
+		HAL_GPIO_WritePin(DOOR_1_RED_LED.GPIOx, DOOR_1_RED_LED.GPIO_Pin, 0);
+		HAL_GPIO_WritePin(DOOR_1_GREEN_LED.GPIOx, DOOR_1_GREEN_LED.GPIO_Pin, 1);
 	} else {
-		HAL_GPIO_WritePin(GPIOA, DOOR_1_RED_LED, 1);
-		HAL_GPIO_WritePin(GPIOA, DOOR_1_GREEN_LED, 0);
+		HAL_GPIO_WritePin(DOOR_1_RED_LED.GPIOx, DOOR_1_RED_LED.GPIO_Pin, 1);
+		HAL_GPIO_WritePin(DOOR_1_GREEN_LED.GPIOx, DOOR_1_GREEN_LED.GPIO_Pin, 0);
 	}
 	return;
 }
@@ -314,7 +341,7 @@ void check_door_2_status(bool *door_1_unlocked_ptr, bool *door_2_unlocked_ptr, b
 	/* Check if door 2 is unlocked. */
 	if (*door_2_unlocked_ptr) {
 
-		bool door_2_opened = HAL_GPIO_ReadPin(GPIOA, DOOR_2_OUTSIDE_SWITCH) == 0;
+		bool door_2_opened = HAL_GPIO_ReadPin(DOOR_2_OUTSIDE_SWITCH.GPIOx, DOOR_2_OUTSIDE_SWITCH.GPIO_Pin) == 0;
 
 		/* Set boolean to tell push to lock to wait for door to close before locking */
 		*waiting_on_door_close_ptr = door_2_opened;
@@ -332,11 +359,11 @@ void check_door_2_status(bool *door_1_unlocked_ptr, bool *door_2_unlocked_ptr, b
 void update_door_2_leds(bool *door_2_unlocked_ptr)
 {
 	if (*door_2_unlocked_ptr) {
-		HAL_GPIO_WritePin(GPIOA, DOOR_2_RED_LED, 0);
-		HAL_GPIO_WritePin(GPIOA, DOOR_2_GREEN_LED, 1);
+		HAL_GPIO_WritePin(DOOR_2_RED_LED.GPIOx, DOOR_2_RED_LED.GPIO_Pin, 0);
+		HAL_GPIO_WritePin(DOOR_2_GREEN_LED.GPIOx, DOOR_2_GREEN_LED.GPIO_Pin, 1);
 	} else {
-		HAL_GPIO_WritePin(GPIOA, DOOR_2_RED_LED, 1);
-		HAL_GPIO_WritePin(GPIOA, DOOR_2_GREEN_LED, 0);
+		HAL_GPIO_WritePin(DOOR_2_RED_LED.GPIOx, DOOR_2_RED_LED.GPIO_Pin, 1);
+		HAL_GPIO_WritePin(DOOR_2_GREEN_LED.GPIOx, DOOR_2_GREEN_LED.GPIO_Pin, 0);
 	}
 	return;
 }
